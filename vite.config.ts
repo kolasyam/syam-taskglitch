2fae9bac9bcb229b74f7003a04b82d88328a0a79
kolasyam <syammanikantakola426@gmail.com>
2026-01-03 11:55:14 +0530

    build error resolved

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// ESM-safe __dirname replacement
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import path from 'node:path';
 
 export default defineConfig({
   plugins: [react()],
